0579abdf6a5abe80eed6b7c2d8ee1e7a114fd463
kureha001 <member@kureha.info>
2025-06-10 23:22:56 +0900

    作り初め

---
+++ ゲーム/saurus/main.py
@@ -0,0 +1,11 @@
+#┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+#┃Pythonで実行          ： python ./main.py
+#┃パッケージファイル化  ： pyxel package ./ main.py
+#┃HTML化                ： pyxel app2html CHAPTER6.pyxapp
+#┃EXE化                 ： pyxel app2exe CHAPTER6.pyxapp
+#┃リソースエディタ起動  ： pyxel edit ".\main\リソース.pyxres"
+#┃Pythonのキャッシュ削除： ./キャッシュ削除.bat ※Windowsで実行
+#┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+import main
+
+main.開始()

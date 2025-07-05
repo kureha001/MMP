98e07fa0ec6d41a0581c71f5d43959d73abb32cc
kureha001 <member@kureha.info>
2025-07-06 03:08:57 +0900

    新規一時保存

--- ゲーム/01_MechBullets/main/汎用部品/テスト.py
+++ ゲーム/01_MechBullets/main/汎用部品/テスト.py
@@ -24,9 +24,9 @@ MMP.通信接続_自動()
 #│
 #◇┐MMPをテストする。
 #　├→（アナログ入力（繰返））
-mode = 320
-if mode == 100:
-    繰返回数 = 1000    # アドレス切替回数
+mode = 1
+if mode == 1:
+    繰返回数 = 300    # アドレス切替回数
     待時間   = 0.05  # ウェイト(秒)
     全権表示 = True  # True:全件表示／False:先頭末尾のみ表示
 
@@ -35,8 +35,8 @@ if mode == 100:
     print("--------------------")
     print("(測定データ)")
     MMP.アナログ設定(
-            2,  # 使用するHC4067の個数(1～4)
-            2, # 使用するHC4067のPin数(1～16)
+            4,  # 使用するHC4067の個数(1～4)
+            1, # 使用するHC4067のPin数(1～16)
             100 # アナログ値の丸め(この数値以下は切り捨て)
             )
     time_start = time.time()
@@ -62,69 +62,8 @@ if mode == 100:
     print("・合計時間   : %02.06f秒"       % (time_diff             ))
     print("・データ平均 : %01.06f秒\n"   % (time_diff/cntTtl        ))
 
-#　├→（ＰＷＤ：ＤＣモータ）
-elif mode ==200:
-    print("ＰＷＤ：ＤＣモータ")
-    番号    = 0     #恐竜ランドのDCモータ
-    最小    = 1800
-    最大    = 3800  #4095がデューティー比100%で最大になる
-    間隔S   = 30
-    間隔E   = -50
-    停止    = 0.2
-
-    for val in range(最小,最大,間隔S):
-        print("PWM:",val)
-        MMP.PWM_VALUE( 番号, val )
-        time.sleep(停止)
-
-    time.sleep(2)
-
-    for val in range(最大,最小,間隔E):
-        print("PWM:",val)
-        MMP.PWM_VALUE( 番号, val )
-        time.sleep(停止)
-    MMP.PWM_VALUE( 番号, -1 )
-
-#　├→（ＰＷＤ：電力供給）
-elif mode == 210:
-    print("ＰＷＤ：電力供給")
-    番号    = 1     #恐竜ランドの音声モジュール＆LED
-    最小    = 4095  #4095:デューティー比100%
-    停止    = 20
-    for i in range(1):
-        MMP.PWM_VALUE( 番号, 最小 )
-        time.sleep(停止)
-        MMP.PWM_VALUE( 番号, 0 )
-
 #　├→（DFPlayer）
-elif mode == 300:
-    print("ｍｐ３プレイヤー")
-
-    print("・ボリューム設定")
-    print(MMP.DFP_Volume(20))
-
-    print("・連続再生")
-    for track in [1, 2, 3]:
-        print(MMP.DFP_Play(track))
-        time.sleep(3)
-
-    print("・１曲目を再生")
-    print(MMP.DFP_Play(1))
-    time.sleep(5)
-
-    print("・一時停止")
-    print(MMP.DFP_Pause())
-    time.sleep(3)
-
-    print("・再開")
-    print(MMP.DFP_Resume())
-    time.sleep(5)
-
-    print("・停止")
-    print(MMP.DFP_Stop())
-
-#　├→（DFPlayer）
-elif mode == 310:
+elif mode == 2:
     print("ｍｐ３プレイヤー")
 
     機器番号 = 1 # 1 or 2

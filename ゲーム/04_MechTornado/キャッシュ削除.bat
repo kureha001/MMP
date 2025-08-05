6a3d4b5e62d987384c0af86fb84630e84e07c610
kureha001 <member@kureha.info>
2025-08-05 14:44:40 +0900

    キャッシュ削除

--- ゲーム/04_MechTornado/キャッシュ削除.bat
+++ ゲーム/04_MechTornado/キャッシュ削除.bat
@@ -4,14 +4,11 @@ RMDIR /s /q .\main\__pycache__\
 
 RMDIR /s /q .\main\データセット\__pycache__\
 
-RMDIR /s /q .\main\コントローラ\更新\__pycache__\
-RMDIR /s /q .\main\コントローラ\描画\__pycache__\
-
-RMDIR /s /q .\main\コントローラ\描画\__pycache__\
+RMDIR /s /q .\main\コントローラ\変更\__pycache__\
+RMDIR /s /q .\main\コントローラ\結果\__pycache__\
 
 RMDIR /s /q .\main\オブジェクト\キャラ\__pycache__\
-RMDIR /s /q .\main\オブジェクト\キャラ\運搬機\__pycache__\
-RMDIR /s /q .\main\オブジェクト\キャラ\発電機\__pycache__\
+RMDIR /s /q .\main\オブジェクト\キャラ\選手\__pycache__\　
 
 RMDIR /s /q .\main\オブジェクト\シーン\__pycache__\
 RMDIR /s /q .\main\オブジェクト\シーン\タイトル画面\__pycache__\

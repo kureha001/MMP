d5f8b1af2d1ab0ff6f5c35a62b3005b367d8e0c6
kureha001 <member@kureha.info>
2025-08-08 14:26:09 +0900

    救出数ではなく、得点で争うよう変更 ・救出点：救出すると加算 ・ボーナス点：高出力で加算

--- ゲーム/02_MechSaurus/main.py
+++ ゲーム/02_MechSaurus/main.py
@@ -1,6 +1,6 @@
 #┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 #┃Pythonで実行          ： python ./main.py
-#┃リソースエディタ起動  ： pyxel edit ".\main\resourece.pyxres"
+#┃リソースエディタ起動  ： pyxel edit ".\main\resource.pyxres"
 #┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 import main
 main.開始()
